87b32f38b47efbc0f896cb9d4df11b28e25807f6
Student <student@Empowrs-MacBook-Pro.local>
2021-06-12 20:27:01 -0500

    fillandstrokechallenge

--- 10-challenge-fill-and-stroke-shapes/Starter/Bullseye/Bullseye/Views/RoundViews.swift
+++ 10-challenge-fill-and-stroke-shapes/Starter/Bullseye/Bullseye/Views/RoundViews.swift
@@ -15,6 +15,10 @@ struct RoundedImageViewStroked: View {
       .font(.title)
       .foregroundColor(Color("TextColor"))
       .frame(width: 56.0, height: 56.0)
+        .overlay(
+        Circle()
+            .strokeBorder(Color("ButtonStrokeColor"),lineWidth: 3)
+        )
   }
 }
 
@@ -24,8 +28,12 @@ struct RoundedImageViewFilled: View {
   var body: some View {
     Image(systemName: systemName)
       .font(.title)
-      .foregroundColor(Color("TextColor"))
+        .foregroundColor(Color("ButtonFilledTextColor"))
       .frame(width: 56.0, height: 56.0)
+        .background(
+        Circle()
+            .fill(Color("ButtonFilledBackgroundColor"))
+        )
   }
 }
 
